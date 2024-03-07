1964ce4cf5702e1a3bc95f29f1c38c09b030feb2
Andrei Gunta <andrei.gunta@agilenetworks.tech>
2024-03-07 16:16:34 +0200

    Course03 commit #1

---
+++ src/main/java/org/siit/course/Course03.java
@@ -0,0 +1,16 @@
+package org.siit.course;
+
+public class Course03 {
+
+    static int x = 5;
+    public static void main(String[] args) {
+        int y = 1;
+
+        String name = "Ana are mere";
+
+        System.out.println("Hello Course 03!");
+        x += y;
+        System.out.println(x);
+    }
+
+}
